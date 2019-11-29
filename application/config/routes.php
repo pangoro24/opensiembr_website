2267f1902acef4c7fedcb058cbf0ce36fad0897d
Juan R. De Leon Osorio <48454427+juanrdlo@users.noreply.github.com>
2019-11-29 08:05:16 -0500

    upadate routes login, register user

--- application/config/routes.php
+++ application/config/routes.php
@@ -10,3 +10,12 @@ $route['app'] = 'front/app';
 $route['how-to-work'] = 'front/how_to_work';
 $route['blog'] = 'front/blog';
 $route['help'] = 'front/help';
+
+// API
+$route['api/register'] = 'api/login/register';
+
+// Administrador
+$route['login'] = 'front/login';
+$route['logout'] = 'front/login/logout';
+$route['register'] = 'front/login/register';
+$route['login/auth'] = 'front/login/auth';

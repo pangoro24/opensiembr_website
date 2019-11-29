bf669dbc5be1048f6314a407913935c50186e94a
Juan R. De Leon Osorio <48454427+juanrdlo@users.noreply.github.com>
2019-11-29 08:14:20 -0500

    add parameter data-aos-once

--- application/views/front/how-to-work.php
+++ application/views/front/how-to-work.php
@@ -7,7 +7,7 @@
 <v-content class="mt-10 mb-10">
 	<v-container>
 		<v-row>
-			<v-col cols="12" md="6" class="mt-12" data-aos="fade-up">
+			<v-col cols="12" md="6" class="mt-12" data-aos="fade-up" data-aos-once="true">
 				<h2 class="mb-5">Step 1</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -17,7 +17,7 @@
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</v-col>
 			<v-col cols="12" md="6">
-				<v-img data-aos="fade-up" data-aos-delay="500" src="https://picsum.photos/200/300" lazy-src="https://picsum.photos/200/300" max-width="100%" max-height="300"></v-img>
+				<v-img data-aos="fade-up" data-aos-delay="200" data-aos-once="true" src="https://picsum.photos/200/300" lazy-src="https://picsum.photos/200/300" max-width="100%" max-height="300"></v-img>
 			</v-col>
 		</v-row>
 	</v-container>
@@ -29,9 +29,9 @@
 	<v-container>
 		<v-row>
 			<v-col cols="12" md="6">
-				<v-img data-aos="fade-up" data-aos-delay="500" src="https://picsum.photos/200/300" lazy-src="https://picsum.photos/200/300" max-width="100%" max-height="300"></v-img>
+				<v-img data-aos="fade-up" data-aos-delay="200" data-aos-once="true" src="https://picsum.photos/200/300" lazy-src="https://picsum.photos/200/300" max-width="100%" max-height="300"></v-img>
 			</v-col>
-			<v-col cols="12" md="6" class="mt-12" data-aos="fade-up">
+			<v-col cols="12" md="6" class="mt-12" data-aos="fade-up" data-aos-once="true">
 				<h2 class="mb-5">Step 2</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -49,7 +49,7 @@
 <v-content class="mt-10 mb-10">
 	<v-container>
 		<v-row>
-			<v-col cols="12" md="6" class="mt-12" data-aos="fade-up">
+			<v-col cols="12" md="6" class="mt-12" data-aos="fade-up" data-aos-once="true">
 				<h2 class="mb-5">Step 3</h2>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -59,7 +59,7 @@
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</v-col>
 			<v-col cols="12" md="6">
-				<v-img data-aos="fade-up" data-aos-delay="500" src="https://picsum.photos/200/300" lazy-src="https://picsum.photos/200/300" max-width="100%" max-height="300"></v-img>
+				<v-img data-aos="fade-up" data-aos-delay="200" data-aos-once="true" src="https://picsum.photos/200/300" lazy-src="https://picsum.photos/200/300" max-width="100%" max-height="300"></v-img>
 			</v-col>
 		</v-row>
 	</v-container>

18baf7568bc1156fdfd606fa31532c9ca53363aa
Juan R. De Leon O <48454427+juanrdlo@users.noreply.github.com>
2019-12-14 19:02:55 -0500

    show last four blog on dashboard

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,12 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends Admin_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('text');
+		$this->load->model('api/blog_model');
+	}
+
 	public function index()
 	{
 		$data = [
 			'title' => 'Dashboard',
 			'section' => 'dashboard',
 			'data'	  => $this->user_model->info(),
+			'all_blogs' => $this->blog_model->_get_limit(4),
 		];
 		$this->load->view('_layouts/admin', $data);
 	}

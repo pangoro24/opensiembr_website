4c76dbf65dfcea124230cb10883d5eba21e2336b
Juan R. De Leon O <48454427+juanrdlo@users.noreply.github.com>
2019-12-21 11:51:47 -0500

    Update User.php

--- application/controllers/admin/User.php
+++ application/controllers/admin/User.php
@@ -14,4 +14,15 @@ class User extends Admin_Controller {
 		$this->load->view('_layouts/admin', $data);
 	}
 
+	public function view($id)
+	{
+		$data = [
+			'title' => 'Dashboard',
+			'section' => 'view',
+			'data'	  => $this->user_model->info(),
+			'user'	=> $this->user_model->_getBy($id)
+		];
+		$this->load->view('_layouts/admin', $data);
+	}
+
 }

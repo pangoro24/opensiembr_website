264c79b3c30be531c90aba43c1ca90b5bdd5db12
Juan R. De Leon O <48454427+juanrdlo@users.noreply.github.com>
2020-01-16 16:22:36 -0500

    Update footer.php

--- application/views/_components/back/footer.php
+++ application/views/_components/back/footer.php
@@ -1,2 +1,2 @@
-<link rel=stylesheet href=" <?= base_url('/assets/back/app.css') ?>">
-<script src="<?= base_url('/assets/back/app.js') ?> " type="text/javascript"></script>
+<link rel=stylesheet href=" <?= base_url('/assets/back/app.css?cb=' .date('U')) ?>">
+<script src="<?= base_url('/assets/back/app.js?cb=' .date('U')) ?> " type="text/javascript"></script>
